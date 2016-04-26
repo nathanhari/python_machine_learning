2ee9a525aa0f7442d3bc349f3ccefc9150028f10
Hari <nathanhari@gmail.com>
2016-04-25 21:35:44 -0400

    Changes and testing for multiclass linear seperability.
    Watned to see how well one-vs-all would work for perceptrons. Tested it
    on the iris dataset and it works quite poorly.

--- perceptron.py
+++ perceptron.py
@@ -22,7 +22,7 @@ class Perceptron:
     self.labels = labels
     self.weights_ = None
   
-  def train(self, XX, yy, n_iter = 100, allowable_erros = 0, 
+  def train(self, XX, yy, n_iter = 100, allowable_errors = 0, 
             reinitialize_weights = False):
     if self.weights_ == None or reinitialize_weights == True:
       self.weights_ = np.zeros(1 + XX.shape[1])
@@ -52,3 +52,29 @@ class Perceptron:
       return(None)
     else:
       return(np.where(self.net_output(X) < 0, -1, 1))
+
+class OneVsAllPerceptron:
+  """
+    Parameters: 
+      labels: list will all the possible labels
+      perceptrons_: all the perceptrons (one for each label)
+  """
+  
+  def __init__(self, labels):
+    self.labels = labels
+    self.perceptrons_ = [Perceptron() for l in self.labels]
+  
+  def train(self, XX, yy, n_iter = 100, allowable_errors = 0,
+            reinitialize_weights = False):
+    all_errors = []
+    for i in range(len(self.labels)):
+      yy_i = np.where(yy == self.labels[i], 1, -1)
+      errors = self.perceptrons_[i].train(XX, yy_i, n_iter=n_iter, 
+                                 allowable_errors=allowable_errors,
+                                 reinitialize_weights=reinitialize_weights)
+      all_errors.append(errors)
+    return(all_errors)
+      
+  def predict(self, X):
+    outputs = [p.net_output(X) for p in self.perceptrons_]
+    return(self.labels[outputs.index(min(outputs))])
